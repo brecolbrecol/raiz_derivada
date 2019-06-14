ba110a27fbd448fbda69173e27710dba91794433
j <jorgeanton@gmail.com>
2019-06-14 03:25:49 +0200

    Cuando encuentra el resultado, para

--- Raiz.java
+++ Raiz.java
@@ -15,19 +15,24 @@ class Raiz {
 		double raiz_aproximada = radicando; 
 		double raiz = Math.sqrt(radicando);
 		double iteraciones = 10;
+		double diferencia_anterior = 1;
+		double diferencia = 0;
 
 		if(args.length == 2) 
 			iteraciones = Float.parseFloat(args[1]);
 
 		for (double i = 0; i < iteraciones ; i++)
 		{
+			diferencia_anterior = diferencia;
 			raiz_aproximada = f(raiz_aproximada, radicando);
+			diferencia = raiz - raiz_aproximada;
+
 			if ((i % 1) == 0)
-			{
-				System.out.println(i + ") " + raiz_aproximada + "   diferencia: " + (raiz - raiz_aproximada) );
-			}
+				System.out.println(i + ") " + raiz_aproximada + "   diferencia: " + diferencia );
+			if (diferencia == 0 || diferencia == diferencia_anterior)
+				break;
 		}
-		System.out.println(iteraciones + ") " + raiz_aproximada + "   diferencia: " + (raiz - raiz_aproximada) );
+		System.out.println("Resultado → " + raiz_aproximada + "   diferencia: " + (raiz - raiz_aproximada) );
 		
 	}
 }
