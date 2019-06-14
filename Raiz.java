0312c80645976087342e5f51872c1a280737b4b6
j <jorgeanton@gmail.com>
2019-06-14 03:12:25 +0200

    Documentacion y posibilidad de especificar el número de iteraciones por parámetro

--- Raiz.java
+++ Raiz.java
@@ -1,18 +1,26 @@
 class Raiz {
 
+	// El valor a puede ser un número fijo, pero cuanto más alejado esté de la raiz,
+	// o pertenezca a un entorno del 0 que haga la división arbitariamente grande,
+	// más iteraciones serán necesarias.
 	public static double f(double a, double c) {
 		return (a + c/a)/2;
 	}
 
-
 	public static void main(String[] args) {
 		double radicando = Float.parseFloat(args[0]);
 		double raiz = Math.sqrt(radicando);
-
-		double a = 2.1;
+		double a = radicando; 
 		double i;
+		double iteraciones = 10;
 
-		for (i = 1; i < 9 ; i++)
+		if(args.length == 1)
+		{
+			iteraciones = Float.parseFloat(args[1]);
+		}
+
+
+		for (i = 1; i < iteraciones ; i++)
 		{
 			a = f(a, radicando);
 			if ((i % 1) == 0)
